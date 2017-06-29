139738fafb784f230f220325aace4c3f51827e6b
Luis Serrano <lserrano@yoogooo.com>
2017-06-28 19:54:27 -0600

    update all

--- SG_app/src/pages/network/network.ts
+++ SG_app/src/pages/network/network.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NavController, NavParams} from 'ionic-angular';
 
 @Component({
   selector: 'page-network',
   templateUrl: 'network.html',
 })
 export class NetworkPage {
-  public users: any;
-  public loader: any;
 
-  constructor(public navCtrl:     NavController, public navParams:   NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
   }
 
